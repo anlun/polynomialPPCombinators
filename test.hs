62e585ddf2cfce8aa1e0c7c5335dc552e1e2a6c4
Podkopaev Anton <podkoav239@gmail.com>
2014-01-21 12:04:30 +0400

    Made test function for mapBURS.

--- test.hs
+++ test.hs
@@ -1,20 +1,32 @@
 import Data.Time
+import Data.List
+import System.IO
+import Control.DeepSeq
 import System.Random
+import Text.Printf
 import qualified MapBURS
 
-treeHeights = [6]--, 7, 8]
+treeHeights = [5, 6, 7]--, 7, 8]
 w = [25, 50, 100, 150]
 
 cross l1 l2 = [(x, y) | x <- l1, y <- l2]
 
 main = do
-  start <- getCurrentTime
   g <- getStdGen
-
   l <- return $ map (\v -> v:"") $ randomRs ('a', 'z') g
-  trees <- return $ map (fst . (MapBURS.heightToDoc l)) treeHeights 
-  results <- return $ map (uncurry MapBURS.pretty) $ cross w trees
-  print $ results
-
-  stop <- getCurrentTime
-  print $ diffUTCTime stop start
+  trees <- return $ map (\h -> (fst (MapBURS.heightToDoc l h), h)) treeHeights
+  a1 <- trees `deepseq` (return "")
+  print a1
+  b <- foldl' (\f (w, (t, h)) -> do
+                c <- f
+                start <- getCurrentTime
+                a <- return $ (++) c $ take 2 $ MapBURS.pretty w t
+                print a
+                stop  <- getCurrentTime
+                printf "width: %d\nheight: %d\n" w h
+                print $ diffUTCTime stop start
+                printf "\n\n"
+                return $ a
+              ) (return "") $ cross w trees
+  print b
+  print "hello"
