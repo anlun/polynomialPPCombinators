05eb73bde0c68df4abb1379bfad44f5d11c20e8a
Podkopaev Anton <podkoav239@gmail.com>
2014-11-20 16:41:51 +0300

    Added depseq for convience

--- lib/Doc.hs
+++ lib/Doc.hs
@@ -4,6 +4,7 @@ module Doc where
 
 import GHC.Generics (Generic)
 import Data.Hashable
+import Control.DeepSeq
 
 data Doc = Text String
          | Indent Int Doc
@@ -12,6 +13,13 @@ data Doc = Text String
          | Doc `Choice` Doc
          deriving (Eq, Ord, Show, Generic)
 instance Hashable Doc
+
+instance NFData Doc where
+  rnf (Text s) = s `deepseq` ()
+  rnf (Indent x d) = x `deepseq` d `deepseq` ()
+  rnf (d1 `Beside` d2) = d1 `deepseq` d2 `deepseq` ()
+  rnf (d1 `Above`  d2) = d1 `deepseq` d2 `deepseq` ()
+  rnf (d1 `Choice` d2) = d1 `deepseq` d2 `deepseq` ()
 
 text   = Text
 indent = Indent
--- tests/randomTree/test.hs
+++ tests/randomTree/test.hs
@@ -14,45 +14,39 @@ import qualified UU.Pretty as UU
 import qualified Pretty
 import qualified PrettyTest
 
-treeHeights = [6, 7, 8, 9, 10, 11]
-w = [25]--[25, 50, 100]--]--, 150]
+treeHeights = [6, 7] --, 8, 9, 10, 11]
+widths = [25]--[25, 50, 100]--]--, 150]
 
-testNumber = 1
-dividingF  = \x -> x `div` 1
+testNumber = 1 :: Integer
+dividingF  = \x -> x `div` testNumber
 
 --fromPicoSeconds :: Integer -> Double
 fromPicoSeconds = \x -> (fromIntegral x) / (10.0 ^ 12)
 
 cross l1 l2 = [(x, y) | y <- l2, x <- l1]
 
-testF s heightToDoc pretty l = do
-  trees <- return $ map (\h ->
-                          snd $ foldl(\(l, (_, xs)) _ ->
-                                       let (t, nl) = heightToDoc l h in
-                                       (nl, (h, t:xs))
-                                     ) (l, (h, [])) [1..testNumber]
-                        ) treeHeights
-  --printf "----------------------\n%s\n----------------------\n" s
-  b <- foldl (\f (h, treeL) -> do
-               --- Fix tree height and tree list
-               c <- f
-               a <- foldl (\f w -> do
-                            --- Fix width
-                            c <- f
-                            start <- getCPUTime
-                            a <- return $ (++) c $ foldl (\f t ->
-                                                           (++) f $ take 2 $ pretty w t
-                                                         ) "" treeL
-                            print a
-                            stop <- getCPUTime
-                            printf "width: %d\nheight: %d\n" w h
-                            printf "Average time: "
-                            print $ fromPicoSeconds $ dividingF $ stop - start
-                            return a 
-                          ) (return "") w
-               return (c ++ a)
-             ) (return "") trees 
-  print b
+treesF heightToDoc treeHeights l =
+  map (\h -> let htd         = \l -> heightToDoc l h
+                 swapAndCons = \xs (t, nl) -> (nl, t:xs)
+                 prFold      = foldl (\(l, xs) _ -> (swapAndCons xs) (htd l))
+                 pr          = prFold (l, []) [1..testNumber] in
+             (h, snd pr)
+  ) treeHeights 
+
+testF s heightToDoc pretty l =
+  let trees = treesF heightToDoc treeHeights l in
+  foldl (\midRes (w, (h, treeL)) ->
+    --let prettyL = deepseq treeL (map (pretty w) treeL) in
+    let prettyL = map (pretty w) treeL in
+    do
+    r <- midRes
+    printf "width: %d\nheight: %d\n" w h
+    printf "Average time: "
+    start <- getCPUTime
+    stop <- deepseq prettyL getCPUTime
+    print $ fromPicoSeconds $ dividingF $ stop - start
+    return ()
+  ) (return () :: IO ()) (cross widths trees)
 
 testHashMap = testF "lib"       PrettyTest.heightToDoc    Pretty.pretty 
 testMapBURS = testF "altLib" AltPrettyTest.heightToDoc AltPretty.pretty
@@ -67,8 +61,8 @@ symbolList = (list 0) ++ symbolList
 main = do
   hSetBuffering stdout NoBuffering
   l <- return symbolList
-  --putStrLn "----------\nHashMap\n-----------\n\n"
-  --testHashMap l
+  putStrLn "----------\nHashMap\n-----------\n\n"
+  testHashMap l
   putStrLn "----------\nMapBURS\n-----------\n\n"
   testMapBURS l
   putStrLn "----------\nClearUU\n-----------\n\n"
